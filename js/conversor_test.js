898ec504e8c70fdad1224c260df83803546f7c46
Luisdavidpm <alu0100816518@ull.edu.es>
2016-03-16 17:36:24 +0000

    Test de Farenheit y Celsius

--- js/conversor_test.js
+++ js/conversor_test.js
@@ -1,9 +1,38 @@
 var expect = chai.expect;
 
-describe("Medida", function() {
-  describe("Medida.convertir", function() {
+describe("Conversor", function() {
+  describe("Medida", function() {
     it("Cambio de f a c", function() {
       expect(Medida.convertir("32f to c")).to.equal("0.00 Celsius");
+    });
+    it("Prueba Matching", function() {
+      expect(Medida.match("32f to c")).to.equal("0");
+    });
+  });
+  describe("Temperatura", function() {
+    it("Creacion de un objeto Farenheit", function() {
+      var source = new Farenheit(32);
+      expect(source.valor).to.equal(32);
+    });
+    it("Prueba de Farenheit a Celsius", function() {
+      var source = new Farenheit(32);
+      expect(source.toCelsius()).to.equal(0);
+    });
+    it("Prueba de Farenheit a Kelvin", function() {
+      var source = new Farenheit(32);
+      expect(source.toKelvin()).to.equal(273.15);
+    });
+    it("Creacion de un objeto Celsius", function() {
+      var source = new Celsius(32);
+      expect(source.valor).to.equal(32);
+    });
+    it("Prueba de Celsius a Farenheit", function() {
+      var source = new Celsius(32);
+      expect(source.toFarenheit()).to.equal(89.6);
+    });
+    it("Prueba de Celsius a Kelvin", function() {
+      var source = new Celsius(32);
+      expect(source.toKelvin()).to.equal(305.15);
     });
   });
 });
