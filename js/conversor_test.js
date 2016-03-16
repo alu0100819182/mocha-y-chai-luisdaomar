63b4409b1b0de9483fa7c1c60c2a56f025be8ee6
alu0100819182 <alu0100819182@ull.edu.es>
2016-03-16 18:24:20 +0000

    Cambios

--- js/conversor_test.js
+++ js/conversor_test.js
@@ -1,14 +1,30 @@
 var expect = chai.expect;
 
 describe("Conversor", function() {
+
+  beforeEach(function() {
+    // create a sandbox
+    sandbox = sinon.sandbox.create();
+
+    // stub some console methods
+    sandbox.stub(window.console, "log");
+    sandbox.stub(window.console, "error");
+  });
+
+  afterEach(function() {
+    // restore the environment as it was before
+    sandbox.restore();
+  });
+
   describe("Medida", function() {
     it("Cambio de f a c", function() {
       expect(Medida.convertir("32f to c")).to.equal("0.00 Celsius");
     });
-    it("Prueba Matching", function() {
+    /*it("Prueba Matching", function() {
       expect(Medida.match("32f to c")).to.equal("0");
-    });
+    });*/
   });
+
   describe("Temperatura", function() {
     it("Creacion de un objeto Farenheit", function() {
       var source = new Farenheit(32);
